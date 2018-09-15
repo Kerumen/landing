21dccb039fc577b43fb0ac0d5cf222a1b3ef2f10
Yann Pringault <yann.pringault@gmail.com>
2018-09-15 17:18:32 +0200

    Fix build with window

--- src/pages/whitepaper.js
+++ src/pages/whitepaper.js
@@ -4,9 +4,10 @@ class WhitePaperPage extends Component {
   constructor(props) {
     super(props)
 
-    window.location.replace(
-      'https://storage.googleapis.com/tokendata/WhitePaperTokenData_31%20aout%202018_English%201.6.27.pdf'
-    )
+    typeof window !== 'undefined' &&
+      window.location.replace(
+        'https://storage.googleapis.com/tokendata/WhitePaperTokenData_31%20aout%202018_English%201.6.27.pdf'
+      )
   }
 
   render() {
