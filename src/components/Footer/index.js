f3d9ad9e5cc4a7927fcc1e62fe0849f0fcc91328
Yann Pringault <yann.pringault@gmail.com>
2018-11-18 19:47:08 +0100

    Update footer with new links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -82,123 +82,144 @@ const Footer = () => (
     </div>
     <div className={styles.columns}>
       <div className={styles.column}>
-        <p>
-          READ TERMS CAREFULLY BEFORE USING THE TDA TOKENS AND the TDA PLATEFORM
-          DESCRIBED HEREIN. BY UTILIZING THE WEBSITE LOCATED AT
-          http://tokendata.ai (“Website”) AND BLOCKCHAIN OFFERED THEREIN, YOU
-          ACKNOWLEDGE THAT YOU HAVE READ TERMS AND THAT YOU AGREE TO BE BOUND BY
-          THEM. IF YOU DO NOT AGREE TO ALL OF THE TERMS OF THIS AGREEMENT, YOU
-          ARE NOT AN AUTHORIZED CONTRIBUTOR OF THE TDA TOKENS AND YOU SHOULD NOT
-          USE THIS WEBSITE NOR ITS PRODUCTS. IN THESE TERMS, "YOU" REFERS TO
-          EITHER THE INDIVIDUAL OR ENTITY ACQUIRING THE TDA TOKENS DIRECTLY AS
-          WELL AS INDIVIDUAL SIGNING ON BEHALF OF SUCH ENTITY.
-        </p>
-        <p>
-          TOKEN DATA RESERVES THE RIGHT TO CHANGE, MODIFY, ADD OR REMOVE
-          PORTIONS OF THESE TERMS AT ANY TIME FOR ANY REASON AFTER YOU AGREE TO
-          THESE TERMS AND YOU AGREE THAT ANY SUCH CHANGES WILL BE BINDING ON
-          YOU. SUCH MODIFICATIONS WILL IN FACT LIKELY HAPPEN AND YOU HEREBY
-          ACKNOWLEDGE THIS FACT. WE SUGGEST THAT YOU REVIEW THESE TERMS
-          PERIODICALLY FOR CHANGES. SUCH CHANGES SHALL BE EFFECTIVE IMMEDIATELY
-          UPON POSTING ON THE WEBSITE. YOU ACKNOWLEDGE THAT BY ACCESSING THE
-          WEBSITE AFTER WE HAVE POSTED CHANGES TO THESE TERMS, IT SHALL BE
-          CONSIDERED THAT YOU WERE ACQUAINTED WITH AND YOU AGREE WITH THE
-          MODIFIED TERMS.
-        </p>
-        <p>
-          THESE TERMS SHALL GOVERN THE CONTRIBUTION PROCESS TO THE TOKEN DATA
-          PROJECT AND DISTRIBUTION OF THE TDA TOKENS. THE TDA TOKENS ARE
-          CRYPTOGRAPHIC TOKENS, BASED ON ETHEREUM BLOCKCHAIN NETWORK WHICH WILL
-          BE THE TOKEN OF THE CROWDSALE ALLOWING CONTRIBUTORS TO ACCESS TO A
-          NUMBER OF UTILITY FUNCTIONS ON THE TDA PLATEFORM.
-        </p>
-        <p>
-          TDA TOKENS DO NOT PRESENT ANY SHAREHOLDERS RIGHT IN TOKEN DATA, NAMELY
-          THE TDA TOKENS DO NOT PRESENT SHARES IN TOKEN DATA, AND HOLDER OF ANY
-          CRYPTOGRAPHIC TOKENS, ISSUED BY TOKEN DATA IS NOT ENTITLED TO ANY FORM
-          OF DIVIDEND OR OTHER REVENUE RIGHT, OR VOTING RIGHT IN THE
-          SHAREHOLDERS' MATTERS. THE TDA TOKEN DOES NOT PRESENT A MONETARY CLAIM
-          AGAINST TOKEN DATA AND DOES NOT GENERATE ANY FORM OF PASSIVE INCOME
-          RIGHT. CONSEQUENTLY, YOU HEREBY AGREE AND UNDERSTAND THAT YOU DO NOT
-          EXPECT ANY PROFITS DERIVED FROM THE PURCHASE OF THE TDA TOKENS, BASED
-          ON THE WORK OF TOKEN DATA.
-        </p>
-        <p>
-          YOU EXPLICITLY CONFIRM THAT YOU ARE AWARE THAT ACQUISITION OF THE TDA
-          TOKENS DOES NOT GRANT YOU A RIGHT TO REQUEST PAYMENT OF ANY KIND, BUT
-          MERELY A RIGHT TO POTENTIALLY USE THE TDA TOKENS WITHIN THE TDA
-          PLATEFORM, AS IT IS OR WILL BE DEFINED IN THE WHITEPAPER AND THESE
-          TERMS. YOU EXPLICITLY CONFIRM THAT YOU ARE AWARE OF A POTENTIAL RISK
-          THAT THE TDA TOKENS LOSE THEIR VALUE COMPLETELY AND YOU CONFIRM THAT
-          YOU ARE ACQUIRING THE TDA TOKENS FOR PERSONAL USE AND NOT FOR
-          INVESTMENT OR SPECULATIVE REASONS.
-        </p>
-        <p>
-          THE TDA TOKENS ARE AVAILABLE TO PURCHASERS IN EXCHANGE FOR BTC, ETH,
-          BCC, LTC AND TOKEN DATA DOES IN NO WAY AND WILL ALSO NOT IN THE FUTURE
-          PROVIDE EXCHANGE OF THE TDA TOKENS FOR FIAT CURRENCY. TOKEN DATA ALSO
-          DOES NOT PROVIDE CUSTODIAL OR WALLET SERVICES FOR THE TOKENS.
-        </p>
+        <div className={styles.terms}>
+          <p>
+            Please read carefully all documentation materials (website,
+            whitepaper, one pager, deck, terms of use, contributions terms) from
+            start to finish before proceed. Participating in the crowdsale of
+            tokendata “tda” tokens may result in significant or complete loss of
+            funds.
+          </p>
+          <p>
+            The documentation materials contained herein is subject to change.
+            consequently, updated versions of this documentation materials will
+            be regularly published by tokendata on its website www.tokendata.ai
+          </p>
+          <p>
+            While tokendata made every reasonable effort to ensure that the
+            information relating to its business and project contained in these
+            documentation materials is accurate and up to date, this information
+            may not be considered as exhaustive and these documentation
+            materials may contain omissions or inaccuracies.
+          </p>
+          <p>
+            These documentation material is for information purposes only.
+            nothing in these documentation material shall be deemed to
+            constitute a prospectus of any sort of a solicitation for
+            investment.
+          </p>
+          <p>
+            The publication or circulation of these documentation materials in
+            certain jurisdiction may be restricted by law or regulation. nothing
+            published by, or republished from, tokendata or any of its
+            subsidiaries should be interpreted as investment advice. tokendata
+            is in no way providing trading or investment advice. please consult
+            with your appropriate licensed professional before making any
+            financial transactions, including any investments related to ideas
+            or opinions expressed, past, present, or future by the
+            aforementioned entities and any future entities that may operate
+            under the parent entities. tokendata does not intend to express
+            financial, legal, tax, or any other advice and any conclusions drawn
+            from statements made by, or on, tokendata shall not be deemed to
+            constitute advice in any jurisdiction.
+          </p>
+          <p>
+            The document is not composed in accordance with, and is not subject
+            to, laws or regulations of any jurisdiction which are designed to
+            protect investors. certain statements, estimates, and financial
+            information contained within these documentation materials
+            constitute forward-looking, or pro-forma statements. such statements
+            or information involve known and unknown risks and uncertainties
+            which may cause actual events or results to differ materially from
+            the estimates or the results implied or expressed in such
+            forward-looking statements.
+          </p>
+        </div>
       </div>
       <div className={styles.column}>
-        <p>
-          TDA TOKENS ARE DIGITAL UTILITY TOKENS WITH NO RIGHTS TO OWNERSHIP,
-          DIVIDENDS, DISTRIBUTIONS OF ASSETS, VOTING OR OTHER RIGHTS EXCEPT FOR
-          THE IMMEDIATE USE AS A TOKEN TO BE USED ON THE TOKEN DATA PLATEFORM.
-          THIS DOCUMENT OR ANY OTHER DOCUMENT, PRODUCED AND SIGNED BY TOKEN DATA
-          DOES NOT CONSTITUTE AN OFFER OR SOLICITATION TO SELL SHARES OR
-          “SECURITIES” OR OTHER INVESTMENTS OF ANY KIND, WHETHER IN TOKEN DATA
-          OR THE WEBSITE OR OTHERWISE. NONE OF THE INFORMATION OR ANALYSES
-          PRESENTED ARE INTENDED TO FORM THE BASIS FOR ANY INVESTMENT DECISION,
-          AND NO SPECIFIC RECOMMENDATIONS ARE INTENDED, AND TOKEN DATA SERVICES
-          AND THE WEBSITE ARE NOT, DO NOT OFFER AND SHALL NOT BE CONSTRUED AS
-          INVESTMENT OR FINANCIAL PRODUCTS. ACCORDINGLY, THIS DOCUMENT DOES NOT
-          CONSTITUTE INVESTMENT ADVICE OR COUNSEL OR SOLICITATION FOR INVESTMENT
-          IN ANY SECURITY OR OTHER INVESTMENT INSTRUMENT AND SHALL NOT BE
-          CONSTRUED IN THAT WAY. THIS DOCUMENT DOES NOT CONSTITUTE OR FORM PART
-          OF, AND SHOULD NOT BE CONSTRUED AS ANY OFFER FOR SALE OR SUBSCRIPTION
-          OF, OR ANY INVITATION TO OFFER TO BUY OR SUBSCRIBE FOR ANY SECURITIES
-          OR OTHER INVESTMENT.
-        </p>
-        <p>
-          ACQUISITION OF TDA TOKENS FROM TOKEN DATA DOES NOT PRESENT AN EXCHANGE
-          OF CRYPTOCURRENCIES FOR ANY FORM OF ORDINARY SHARES OR OTHER
-          INVESTMENT IN TOKEN DATA, ITS AFFILIATES OR THE WEBSITE, AND HOLDER OF
-          ANY CRYPTOGRAPHIC TOKENS, ISSUED BY TOKEN DATA IS NOT ENTITLED TO ANY
-          FORM OF DIVIDEND, DISTRIBUTION OR RIGHT TO ASSETS OR PROFITS OF ANY
-          KIND. HOLDERS OF TDA TOKENS ARE ONLY ENTITLED TO THE USE OF THE TDA
-          PLATEFORM AND SOFTWARE IN ACCORDANCE WITH THE TERMS SET OUT HEREIN.
-          TOKEN DATA ALSO EXPLICITLY WARNS YOU AND YOU EXPLICITLY AGREE THAT
-          WITHDRAWAL OF THE TOKENS UPON THEIR ACQUIRING MIGHT BE CONDITIONED BY
-          REQUEST THAT EACH CONTRIBUTOR PASSES THE IDENTIFICATION PROCESS (KNOW
-          YOUR CUSTOMER) AND THAT WITHDRAWAL OF THE TDA TOKENS MIGHT NOT BE
-          POSSIBLE (MAY BE REFUSED BY TOKEN DATA) UNTIL EACH CONTRIBUTOR
-          COMPLETES THE IDENTIFICATION PROCESS.
-        </p>
-        <p>
-          ANY PERSON OR ENTITY, INCLUDING ANYONE ACTING ON ITS BEHALF, BEING
-          BASED, DOMICILED, LOCATED OR INCORPORATED IN THE UNITED STATES OF
-          AMERICA, NEW ZEALAND, THE REPUBLIC OF NORTH KOREA, OR PEOPLE’S
-          REPUBLIC OF CHINA, SHALL NOT USE THE WEBSITE OR ACQUIRE OR USE TDA
-          TOKENS AND SHALL LEAVE THE WEBSITE IMMEDIATELY, OTHERWISE THIS PERSON
-          ASSUMES ALL THE RESPONSIBILITY AND LIABILITY ARISING FROM THE
-          CONTINUED USE OF THESE WEBSITE AND/OR TDA TOKENS AND SHALL INDEMNIFY
-          TOKEN DATA FOR ANY DAMAGES ARISING OUT OF THIS.
-        </p>
-        <p>
-          TOKEN DATA EXPRESSLY DISCLAIMS ANY AND ALL RESPONSIBILITY, LIABILITY
-          AND POSSIBLE THIRD- PARTY LIABILITY FOR ANY DIRECT OR CONSEQUENTIAL
-          LOSS, PURE FINANCIAL LOSS, LOSS OF PROFIT OR DAMAGE OF ANY KIND
-          WHATSOEVER ARISING DIRECTLY OR INDIRECTLY FROM: (I) RELIANCE ON ANY
-          INFORMATION CONTAINED IN THIS DOCUMENT, (II) ANY ERROR, OMISSION OR
-          INACCURACY IN ANY SUCH INFORMATION OR (III) ANY ACTION RESULTING
-          THEREFROM, (IV) USAGE OR ACQUISITION OF PRODUCTS, AVAILABLE THROUGH
-          THE WEBSITE.
-        </p>
+        <div className={styles.terms}>
+          <p>
+            TDA tokens may be qualified financial securities advice in some
+            jurisdictions. therefore, tda tokens are not available to citizens,
+            residents (tax or otherwise), green card holders, and other persons,
+            both natural and legal of: republic of china, the united states of
+            america, south korea, north korea, or any jurisdiction that
+            prohibits the possession, dissemination or communication of the
+            documentation materials and / or prohibits participation in the tda
+            token sale offering, purchase, possession and / or exchange of tda
+            tokens or any similar activity are not permitted to participate in
+            the token sale. in some jurisdictions where tda tokens may be
+            qualified financial securities, the selling is{' '}
+            <a
+              href="https://gallery.mailchimp.com/4a11c93b3bc3fb3fa2bb52e8b/files/78b1582c-4f47-4bc6-bc84-c537bc30b3bc/Selling_Restrictions_TokenData.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.termsLink}
+            >
+              under restriction
+            </a>{' '}
+            to professional (accredited) investors. please contact us at
+            investor-relations@tokendata.ai
+          </p>
+          <p>
+            Tokendata reserves the right to change, modify, add or remove
+            portions of these documentations materials at any time for any
+            reason after you agree to these terms and you agree that any such
+            changes will be binding on you. such modifications will in fact
+            likely happen and you hereby acknowledge this fact. we suggest that
+            you review these terms periodically for changes. such changes shall
+            be effective immediately upon posting on the website. you
+            acknowledge that by accessing the website after we have posted
+            changes to these terms, it shall be considered that you were
+            acquainted with and you agree with the modified terms.
+          </p>
+          <p>
+            The Initial Coin Offering (hereafter, the “ICO”) project presented
+            by TokenData (hereafter, the “Company”) is an unregulated
+            fundraising operation. It poses several risks to buyers, in
+            particular, that of losing all amounts traded for tokens issued by
+            the Company (hereafter, the “TDA”).
+          </p>
+          <p>
+            You acknowledge and agree that there are risks associated with
+            purchasing, holding, and using TDA Tokens in connection with the
+            Company’s product, services and platform developed for such products
+            and/or services (hereafter, the “Platform”), as disclosed and
+            explained in this White Paper and in the Terms and Condition
+            available at sto-tokendata.ai. If you have any questions regarding
+            these risks, please contact us at relation_investor@tokendata.ai.
+          </p>
+          <p>
+            BY PURCHASING TDA TOKENS, YOU EXPRESSLY ACKNOWLEDGE AND ASSUME THESE
+            RISKS.
+          </p>
+          <p>
+            Only people who are fully aware of these risks should participate in
+            the ICO. Note also that the ICO excludes certain groups of people
+            such as “U.S. Person” (within the meaning of “Regulation S” of the
+            Securities Act 1933 under U.S. law) and private individuals acting
+            on a non-professional basis as simple consumers (within the meaning
+            of EU Directive 2011/83/EU of the European Parliament and of the
+            Council of 25 October 2011 on consumer rights).
+          </p>
+        </div>
         <div className={styles.links}>
-          <div onClick={openMailchimpModal} className={styles.link}>
-            Terms & Conditions
-          </div>
+          <a
+            href="https://gallery.mailchimp.com/4a11c93b3bc3fb3fa2bb52e8b/files/babc7047-13ba-48c3-b233-2fdaf529cb7f/Terms_of_Use_Agreement.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            Terms of use
+          </a>
+          <a
+            href="https://gallery.mailchimp.com/4a11c93b3bc3fb3fa2bb52e8b/files/64ed2b30-ce4b-498c-8c60-c6a0bf926c7b/Data_Privacy.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            Data Privacy
+          </a>
           <a href="mailto:hello@tokendata.ai" className={styles.link}>
             Contact Us
           </a>
@@ -211,7 +232,7 @@ const Footer = () => (
             FAQ
           </a>
         </div>
-        <p>© 2018 TokenDATA. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} TokenDATA. All rights reserved.</p>
       </div>
     </div>
   </div>
