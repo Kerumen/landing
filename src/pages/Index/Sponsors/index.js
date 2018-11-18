721d24617cafdf8071ef32b63210567a68961b0b
Yann Pringault <yann.pringault@gmail.com>
2018-11-18 19:25:51 +0100

    Add sponsors (#2)
    * Add sponsors
    
    * Update with retina
    
    * Remove unused images

--- src/pages/Index/Sponsors/index.js
+++ src/pages/Index/Sponsors/index.js
@@ -1,52 +1,39 @@
 import React from 'react'
+import cx from 'classnames'
 
 import Image from '../../../components/Image'
 
 import styles from './styles.module.scss'
 
-const images = [
-  [
-    'bitcoin',
-    'ethereum',
-    'crypto-valley',
-    'wachsman',
-    'kenetic-capital',
-    'riot',
-    'masschallenge',
-    'start-global',
-  ],
-  [
-    'webit-festival',
-    'epfl',
-    'brennpunkt',
-    'un',
-    'microsoft-bizspark',
-    'pioneers-festival',
-    'mme',
-  ],
-  [
-    'ldj-capital',
-    'vaud',
-    'monaco',
-    'validity-labs',
-    'trek',
-    'eit-food',
-    'hello-tomorrow',
-  ],
+const partners = [
+  { name: 'Astek', size: [103, 28] },
+  { name: 'Bankex', size: [125, 34] },
+  { name: 'Coinbase', size: [103, 36] },
+  { name: 'Columbia', size: [55, 59] },
+  { name: 'Crypto Valley', size: [107, 21] },
+  { name: 'EPFL', size: [46, 52] },
+  { name: 'Ethereum', size: [80, 53] },
+  { name: 'GCP', size: [155, 40] },
+  { name: 'Medium', size: [80, 60] },
+  { name: 'Product Hunt', size: [133, 24] },
+  { name: 'Steemit', size: [70, 53] },
+  { name: 'SwissLegal', size: [112, 56] },
+  { name: 'TNW', size: [80, 34] },
+  { name: 'Vaud', size: [80, 16] },
+  { name: 'WeStart', size: [76, 20] },
 ]
 
 const Sponsors = () => (
   <div className={styles.container}>
-    {images.map((row, index) => (
-      <div key={index} className={styles.row}>
-        {row.map(image => (
-          <Image
-            key={image}
-            type="sponsors"
-            name={image}
-            className={styles.image}
-          />
-        ))}
+    {partners.map(({ name, size: [width, height] }) => (
+      <div className={styles.image}>
+        <Image
+          key={name}
+          type="sponsors"
+          name={name.replace(/ /g, '-').toLowerCase()}
+          retina={true}
+          // style={{ width, height }}
+        />
       </div>
     ))}
   </div>
