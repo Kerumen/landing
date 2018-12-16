4775e3a6313257e8e7d859f164df98c520491fc1
Yann Pringault <yann.pringault@gmail.com>
2018-12-16 14:44:09 +0100

    Beta -> Get early access

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -127,7 +127,7 @@ class Header extends Component {
               href="https://tokendata.typeform.com/to/eUeyVn/"
               color={color}
             >
-              Beta
+              Get early access
             </MenuLink>
           </div>
           <div className={styles.mobileMenuToggle} onClick={this.toggleMenu}>
@@ -162,7 +162,7 @@ class Header extends Component {
                 rel="noopener noreferrer"
                 className={styles.mobileMenuLink}
               >
-                Beta
+                Get early access
               </a>
             </div>
           )}
